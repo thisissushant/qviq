ceb17cbf6983eeef8ad47afc3998c47828daf833
Sushant Singh <sushantbibhu@gmail.com>
2024-05-02 16:15:31 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="font-bold text-sky-500">Hello QviQ</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
