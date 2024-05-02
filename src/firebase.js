19405ab754d1be94769c55df5e724acb113c79f6
Sushant Singh <sushantbibhu@gmail.com>
2024-05-02 16:30:40 +0530

    Add firebase as a dependency

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwcoYoO8YklUoo270s0n5EMdfpuiRDms4",
+  authDomain: "qviq-41276.firebaseapp.com",
+  projectId: "qviq-41276",
+  storageBucket: "qviq-41276.appspot.com",
+  messagingSenderId: "242190126727",
+  appId: "1:242190126727:web:ea6efbad6df3a0cfdaf222",
+};
+
+const app = initializeApp(firebaseConfig);
